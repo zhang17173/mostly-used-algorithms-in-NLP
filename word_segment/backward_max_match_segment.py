abc6818344a9a6aa7fc653c6efafce07dcdf5411
Zhang Shiwei <zhangshiwei17173@outlook.com>
2020-04-15 11:59:56 +0800

    新增后向最大词典匹配算法

--- word_segment/backward_max_match_segment.py
+++ word_segment/backward_max_match_segment.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # _*_coding:utf-8 _*_
 # @Author:Zhang Shiwei
-# @Date  :2020/4/13
+# @Date  :2020/4/15
 
 """
-基于词典的前向最大匹配分词算法
+基于词典的后向最大匹配分词算法
 """
 
 
@@ -27,26 +27,31 @@ def add_dict(dictfile):
 
 def segment(rawfile, dictfile):
     """
-    前向最大匹配分词
+    后向最大匹配分词
     :param rawfile: 待分词文本
     :param dictfile: 词典文件
     :return:
     """
     dictionary, max_length = add_dict(dictfile)
     f_raw = open(rawfile, "r", encoding="utf-8")
-    f_result = open("seg_results/forward.txt", "w", encoding="utf-8")  # 分词结果
+    f_result = open("seg_results/backward.txt", "w", encoding="utf-8")
     for line in f_raw.readlines():
+        words = list()  # 存储一句话分好的单词
         line = line.strip()
         while len(line) > 0:
             max_len = len(line) if len(line) < max_length else max_length  # 当前尝试的分词长度
-            try_word = line[:max_len]
-            while try_word not in dictionary:
+            try_word = line[-max_len:]
+            while try_word not in dictionary:  # 不断在词典中查找当前单词
                 if len(try_word) == 1:
                     break
                 else:
-                    try_word = try_word[:len(try_word) - 1]
-            f_result.write(try_word + "|")
-            line = line[len(try_word):]
+                    try_word = try_word[1:]
+            words.append(try_word)
+            line = line[:-len(try_word)]
+        # 写入结果
+        while words:
+            word = words.pop()
+            f_result.write(word + "|")
         f_result.write("\n")
 
     f_raw.close()
